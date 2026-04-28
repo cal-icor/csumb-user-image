b6bbd2ee3e2ca75013c312ab5734da2a6b39c297
shane knapp <incomplete@gmail.com>
2026-04-28 14:13:07 -0700

    fix name

--- molecularecology-install.sh
+++ molecularecology-install.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 # mamba env create -q -n molecularecology -f /tmp/molecularecology.yaml
 mamba env create \
-  --name rachis-qiime2-2026.4 \
+  --name molecularecology \
   --file https://raw.githubusercontent.com/qiime2/distributions/refs/heads/dev/2026.4/qiime2/released/rachis-qiime2-linux-64-conda.yml
 mamba clean -afy
