2b6876042a777cdbb253af4b4bd376a7452aad70
shane knapp <incomplete@gmail.com>
2026-08-18 13:54:47 -0700

    refactoring csumb image builds

--- bioinformatics-install.sh
+++ bioinformatics-install.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-mamba env create -q -n bioinformatics -f /tmp/bioinformatics.yaml
+mamba env create -q -f /tmp/bioinformatics.yaml -p ${BIOINFORMATICS_DIR}
 mamba clean -afy
--- molecularecology-install.sh
+++ molecularecology-install.sh
@@ -3,7 +3,11 @@
 set -euo pipefail
 
 # mamba env create -q -n molecularecology -f /tmp/molecularecology.yaml
+ENV_DEF="https://raw.githubusercontent.com/qiime2/distributions/refs/heads/dev/2026.4/qiime2/released/rachis-qiime2-linux-64-conda.yml"
+curl --silent --location --fail ${ENV_DEF} > /tmp/$(basename ${ENV_DEF})
+ENV_FILE="/tmp/$(basename ${ENV_DEF})"
+sed -i '1iname: molecularecology' ${ENV_FILE}
 mamba env create \
-  --name molecularecology \
-  --file https://raw.githubusercontent.com/qiime2/distributions/refs/heads/dev/2026.4/qiime2/released/rachis-qiime2-linux-64-conda.yml
+  --file ${ENV_FILE} \
+  --prefix ${MOLECULARECOLOGY_DIR}
 mamba clean -afy
