dfd102124ff5b757f0b45e5ebd41fcb3b757c2b9
shane knapp <incomplete@gmail.com>
2026-08-19 13:24:37 -0700

    maybe this will fix it?

--- molecularecology-install.sh
+++ molecularecology-install.sh
@@ -6,6 +6,5 @@ ENV_DEF="https://raw.githubusercontent.com/qiime2/distributions/refs/heads/dev/2
 curl --silent --location --fail ${ENV_DEF} > /tmp/$(basename ${ENV_DEF})
 ENV_FILE="/tmp/$(basename ${ENV_DEF})"
 sed -i '1iname: molecularecology' ${ENV_FILE}
-/srv/conda/bin/mamba env create \
-  --file ${ENV_FILE} \
-/srv/conda/bin/mamba clean -afy
+mamba env create -q -f ${ENV_FILE}
+mamba clean -afy
